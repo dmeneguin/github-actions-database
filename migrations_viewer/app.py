d24c40d3a591e8d4d2c24ab73a6d75c8ce280c77
dmeneguin <meneguin.daniel@gmail.com>
2022-09-06 14:15:44 -0300

    added viewer

---
+++ migrations_viewer/app.py
@@ -0,0 +1,40 @@
+import os
+import urllib.request
+from flask import Flask
+from flask import jsonify
+import sqlite3
+app = Flask(__name__)
+
+
+
+def download_migrations_db():
+    url = 'https://github.com/dmeneguin/github-actions-database/blob/master/migrations.db?raw=true'
+    file_name, headers = urllib.request.urlretrieve(url)
+    return file_name
+
+
+@app.route('/migrations')
+def downloadFile ():
+    path = download_migrations_db()
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT * FROM migrations;
+    """)
+
+    response = []
+    for row in cursor.fetchall():
+        response.append(row)
+
+    conn.close()
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
