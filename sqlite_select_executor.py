1838fa0a85530a76a0d20ff7e2cda6bf8fc0dec0
Daniel Meneguin Barbosa <meneguin.daniel@gmail.com>
2022-09-21 12:12:53 -0300

    Update sqlite_select_executor.py

--- sqlite_select_executor.py
+++ sqlite_select_executor.py
@@ -3,7 +3,8 @@ import sys
 import json
 
 print(sys.argv[1:])
-valid_inputs = json.loads(sys.argv[1:])
+valid_inputs = json.loads(sys.argv[1:][0])
+print(valid_inputs[0].get('name'))
 print(valid_inputs[0].get('name'))
 
 conn = sqlite3.connect('migrations.db')
